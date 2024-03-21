4b9238753f082825f14592f5987a9ac9a85be37e
beemaster <ugor.finno@yandex.ru>
2024-03-21 09:43:09 +0500

    Modify task1 in home work 4

--- home_work_4/task_1.sql
+++ home_work_4/task_1.sql
@@ -1,11 +1,3 @@
-SELECT COUNT(*) FROM profiles p
--- INNER JOIN users u ON p.user_id = u.id
-INNER JOIN media m ON p.user_id = m.user_id
-INNER JOIN likes l ON m.id = l.media_id
--- WHERE p.gender = 'f';
--- SHOW FULL TABLES;
--- SELECT * FROM likes;
--- SELECT * FROM profiles;
 SELECT COUNT(*) -- количество лайков
 FROM likes l
 JOIN media m ON l.media_id = m.id
